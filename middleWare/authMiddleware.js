fc91160faf627ba37ef87c059519543d1aa58f8d
ifekel <ifeanyionyekwelu786@gmail.com>
2023-06-09 16:32:00 +0100

    Checked

--- middleWare/authMiddleware.js
+++ middleWare/authMiddleware.js
@@ -1,33 +1,39 @@
-const asyncHandler = require("express-async-handler");
-const jwt = require('jsonwebtoken');
-const User = require("../models/userModel.js");
+const asyncHandler = require('express-async-handler')
+const jwt = require('jsonwebtoken')
+const User = require('../models/userModel.js')
 
 const protect = asyncHandler(async (req, res, next) => {
-    try {
-        const token = req.cookies.token;
-        if (!token) {
-            res.status(401)
-            throw new Error("Not Authorized, please login")
-        }
-
-        // verify the token
-        const decryptedToken = jwt.verify(token, process.env.jwtSecret)
-        // get user id from token
-        const user = await User.findById(decryptedToken.id).select("-password")
-            .populate('downlines.package.ID', 'name')
-
-        if (!user) {
-            res.status(401)
-            throw new Error("user not found")
-        }
-
-        req.user = user
-        next();
-    } catch (error) {
-        res.status(401)
-        throw new Error("Not Authorized, please login")
+  try {
+    const token = req.cookies.token
+    if (!token) {
+      res.status(401)
+      throw new Error('Not Authorized, please login')
     }
-});
 
+    // verify the token
+    const decryptedToken = jwt.verify(token, process.env.jwtSecret)
+    // get user id from token
+    const user = await User.findById(decryptedToken.id)
+      .select('-password')
+      .populate('downlines.package.ID', 'name')
 
-module.exports = protect;
+    if (!user) {
+      res.status(401)
+      throw new Error('user not found')
+    }
+
+    // Populate user's transaction history
+    const transactionHistory = await Transaction.find({ user: user._id })
+
+    req.user = {
+      ...user._doc,
+      transactionHistory: transactionHistory
+    }
+    next()
+  } catch (error) {
+    res.status(401)
+    throw new Error('Not Authorized, please login')
+  }
+})
+
+module.exports = protect
