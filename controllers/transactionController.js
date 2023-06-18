4143bf5630d517248cf29911efda441cf0a43a3a
ifekel <ifeanyionyekwelu786@gmail.com>
2023-06-18 12:26:44 +0100

    Working purchaseData controller

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -13,7 +13,7 @@ const currentHour = currentDate.getHours();
 const currentMinutes = currentDate.getMinutes();
 
 const purchaseAirtime = asyncHandler(async (req, res) => {
-  const { network, phoneNumber, amount, transactionId } = req.body
+  const { network, phoneNumber, amount } = req.body
 
   // validate data
   if (!network || !phoneNumber || !amount) {
@@ -56,7 +56,7 @@ const purchaseAirtime = asyncHandler(async (req, res) => {
 
     // Add the bonus amount to the user's balance
     const user = await User.findById(req.user.id)
-    user.commissionBalance += Number(bonusAmount)
+    user.walletBalance += Number(bonusAmount)
     await user.save()
 
     // Get the user's package details
@@ -68,7 +68,7 @@ const purchaseAirtime = asyncHandler(async (req, res) => {
       userPackage.transaction &&
       userPackage.transaction.transactionLevels > 0
     ) {
-      const transactionLevels = userPackage.transaction.transactionLevels
+      const transactionLevels = userPackage.transaction.level
 
       // Determine the bonus levels based on the user's package and referralBonusLevel
       const bonusLevels = Math.min(
@@ -91,7 +91,7 @@ const purchaseAirtime = asyncHandler(async (req, res) => {
             (amount * userPackage.transaction.transactionProfit) / 100
           ).toFixed(2)
 
-          upline.commissionBalance += Number(transactionProfit)
+          upline.walletBalanceBalance += Number(transactionProfit)
           await upline.save()
         }
 
@@ -112,30 +112,219 @@ const purchaseAirtime = asyncHandler(async (req, res) => {
   }
 })
 
-const purchaseData = asyncHandler(async (req, res) => {
-  const { network, networkPlan, phoneNumber, amount } = req.body
+// const purchaseData = asyncHandler(async (req, res) => {
+//   const { network, networkPlan, phoneNumber, amount } = req.body
 
-  console.log('network:', network)
-  console.log('dataPlan:', networkPlan)
-  console.log('phoneNumber:', phoneNumber)
+//   console.log('network:', network)
+//   console.log('dataPlan:', networkPlan)
+//   console.log('phoneNumber:', phoneNumber)
+
+//   // Validate data
+//   if (!network || !phoneNumber || !networkPlan) {
+//     res.status(400)
+//     throw new Error('Please fill in all fields')
+//   }
+
+//   // // Find the data plan from the database
+//   // const dataPlanEntry = await DataPlan.findOne({
+//   //   networkPlan: network,
+//   //   'plans.productCode': dataPlan
+//   // })
+
+//   // Define the profit for each mobile network
+//   const matchingPlan = await DataPlan.findOne({
+//     "plans.productCode": networkPlan
+//   });
+//   // const selectedPlan = dataPlanEntry.plans.find(
+//   //   plan => plan.productCode === dataPlan
+//   // )
+//   console.log(matchingPlan)
+
+//   // Check if the requested plan is available
+//   if (!matchingPlan) {
+//     res.status(400);
+//     throw new Error("Selected Plan not available");
+//   }
+
+//   const plan = matchingPlan.plans.find((plan) => plan.productCode === networkPlan);
+//   const profit = plan.difference;
+
+//   // Check if the user has sufficient balance in their wallet
+//   if (req.user.walletBalance < amount) {
+//     return res.status(400).json({
+//       message: 'Insufficient wallet balance'
+//     });
+//   }
+
+//   // Calculate the bonus amount based on the profit
+//   const bonusAmount = (profit * 0.4).toFixed(2);
+//   const transactionId = req.user.username + `${currentYear}${currentMonth}${currentHour}${currentMinutes}`;
+
+//   // console.log('dataPlanEntry:', dataPlanEntry)
+
+//   // if (!dataPlanEntry) {
+//   //   res.status(400)
+//   //   throw new Error('Invalid data plan')
+//   // }
+
+//   // const selectedPlan = dataPlanEntry.plans.find(
+//   //   plan => plan.productCode === dataPlan
+//   // )
+
+//   // console.log('selectedPlan:', selectedPlan)
+
+//   // if (!selectedPlan) {
+//   //   res.status(400)
+//   //   throw new Error('Invalid data plan')
+//   // }
+
+//   // const { productAmount, productName } = selectedPlan
+
+//   // console.log('productAmount:', productAmount)
+//   // console.log('productName:', productName)
+
+//   // Simulate dummy API response
+//   const dummyApiResponse = {
+//     statuscode: 200 // Simulate a successful transaction
+//     // Add other properties if needed
+//   }
+
+//   console.log('dummyApiResponse:', dummyApiResponse)
+
+//   // Check if the dummy API transaction was successful
+//   if (dummyApiResponse.statuscode === 200) {
+//     // Retrieve the user from the database
+//     const user = await User.findById(req.user.id)
+//     console.log('user:', user)
+//     if (!user) {
+//       res.status(400)
+//       throw new Error('User not found')
+//     }
+
+//     // Check if the user's balance is sufficient
+//     if (user.walletBalance < amount) {
+//       res.status(400)
+//       throw new Error('Insufficient balance')
+//     }
+
+//     // Deduct the product price from the user's balance
+//     user.walletBalance -= amount
+//     // Get the user's package transaction level and percentage
+//     const userPackage = await Package.findOne({ _id: user.package.ID })
+//     console.log('userPackage:', userPackage)
+//     const userTransactionLevel = userPackage.transaction.level
+//     const userTransactionPercentage = user.userTransactionPercent
+
+//     console.log('userTransactionLevel:', userTransactionLevel)
+//     console.log('userTransactionPercentage:', userTransactionPercentage)
+
+//     // // Calculate the bonus amount based on the product amount and transaction percentage
+//     // const bonusAmount = (
+//     //   (amount * userTransactionPercentage) /
+//     //   100
+//     // ).toFixed(2)
+
+//     // Add the bonus amount to the user's balance
+//     user.walletBalance += Number(bonusAmount)
+//     await user.save()
+//     console.log('bonusAmount:', bonusAmount)
+
+//     // Check if the package has transaction levels defined
+//     console.log(userPackage);
+//     console.log(userPackage.transaction);
+//     console.log(userPackage.transaction.level);
+//     if (userPackage && userPackage.transaction && userPackage.transaction.level > 0) {
+//       const transactionLevels = userPackage.transaction.level;
+
+//       // Determine the bonus levels based on the user's package and referralBonusLevel
+//       const bonusLevels = Math.min(
+//         transactionLevels,
+//         userPackage.referralBonusLevel
+//       );
+
+//       let uplineUser = user;
+//       for (let i = 1; i <= bonusLevels; i++) {
+//         // Check if upline user exists
+//         if (!uplineUser.upline) {
+//           break; // No more upline to calculate bonuses for
+//         }
+
+//         // Find upline user and update their balance with transaction profit
+//         const upline = await User.findById(uplineUser.upline.ID)
+//         if (upline) {
+//           // Check if the upline user is a basic user
+//           console.log(upline.package.name)
+//           if (upline.package.name === 'Basic' && i !== 1) {
+//             uplineUser = upline.upline;
+//             continue;
+//           }
+
+//           const uplinePercentage = await User.findById(uplineUser.upline.ID).populate({
+//             path: 'package.ID',
+//             select: 'transaction.percentage',
+//           });
+
+//           console.log(uplinePercentage.package.ID.transaction.percentage);
+//           const transactionProfit = Number(
+//             (profit * uplinePercentage.package.ID.transaction.percentage) / 100
+//           ).toFixed(2);
+//           console.log("Upline Tranaction Profit: ", transactionProfit);
+
+//           upline.walletBalance += Number(transactionProfit);
+//           await upline.save();
+//         }
+
+//         uplineUser = upline; // Move to the next upline user
+//       }
+//     }
+
+//     // If the transaction is successful, create a new credit transaction record for the data purchase
+//     const transaction = new Transaction({
+//       user: req.user.id,
+//       amount: amount,
+//       transactionId: transactionId, // Replace with actual transaction ID
+//       transactionType: 'data',
+//       commission: bonusAmount,
+//       status: 'completed',
+//       dataPlan: network,
+//       phoneNumber: phoneNumber,
+//       network: network
+//     })
+
+//     await transaction.save()
+
+//     res.status(200).json({
+//       message: 'Data plan purchased successfully',
+//       bonusAmount
+//     })
+//   } else {
+//     res.status(400).json({
+//       message: 'Failed to purchase data plan'
+//     })
+//   }
+// })
+
+const purchaseData = asyncHandler(async (req, res) => {
+  const { network, networkPlan, phoneNumber, amount } = req.body;
+  console.log('network:', network);
+  console.log('networkPlan:', networkPlan);
+  console.log('phoneNumber:', phoneNumber);
+  console.log('amount:', amount);
 
   // Validate data
   if (!network || !phoneNumber || !networkPlan) {
-    res.status(400)
-    throw new Error('Please fill in all fields')
+    res.status(400);
+    throw new Error('Please fill in all fields');
   }
 
-  // // Find the data plan from the database
-  // const dataPlanEntry = await DataPlan.findOne({
-  //   networkPlan: network,
-  //   'plans.productCode': dataPlan
-  // })
-
-  // Define the profit for each mobile network
+  // Find the data plan from the database
   const matchingPlan = await DataPlan.findOne({
     "plans.productCode": networkPlan
   });
-  console.log(matchingPlan)
+  console.log('matchingPlan:', matchingPlan);
+  const productName = plan.productName.match(/\d+(\.\d+)?/)[0];
+
+  console.log(productName)
 
   // Check if the requested plan is available
   if (!matchingPlan) {
@@ -145,6 +334,8 @@ const purchaseData = asyncHandler(async (req, res) => {
 
   const plan = matchingPlan.plans.find((plan) => plan.productCode === networkPlan);
   const profit = plan.difference;
+  console.log('plan:', plan);
+  console.log('profit:', profit);
 
   // Check if the user has sufficient balance in their wallet
   if (req.user.walletBalance < amount) {
@@ -156,88 +347,64 @@ const purchaseData = asyncHandler(async (req, res) => {
   // Calculate the bonus amount based on the profit
   const bonusAmount = (profit * 0.4).toFixed(2);
   const transactionId = req.user.username + `${currentYear}${currentMonth}${currentHour}${currentMinutes}`;
+  console.log('bonusAmount:', bonusAmount);
+  console.log('transactionId:', transactionId);
 
-  // console.log('dataPlanEntry:', dataPlanEntry)
+  const CKuserId = process.env.CLUB_KONNECT_USER_ID;
+  const apiKey = process.env.CLUB_KONNECT_API_KEY;
+  const apiUrl = `${process.env.CLUB_KONNECT_DATA_URI}?UserID=${CKuserId}&APIKey=${apiKey}&MobileNetwork=${network}&DataPlan=${productName}&MobileNumber=${phoneNumber}&RequestID=${transactionId}`;
+  console.log('apiUrl:', apiUrl);
 
-  // if (!dataPlanEntry) {
-  //   res.status(400)
-  //   throw new Error('Invalid data plan')
-  // }
+  // Make API request to ClubKonnect
+  const response = await axios.post(apiUrl);
+  console.log('response:', response);
 
-  // const selectedPlan = dataPlanEntry.plans.find(
-  //   plan => plan.productCode === dataPlan
-  // )
+  const { status, data } = response;
 
-  // console.log('selectedPlan:', selectedPlan)
-
-  // if (!selectedPlan) {
-  //   res.status(400)
-  //   throw new Error('Invalid data plan')
-  // }
-
-  // const { productAmount, productName } = selectedPlan
-
-  // console.log('productAmount:', productAmount)
-  // console.log('productName:', productName)
-
-  // Simulate dummy API response
-  const dummyApiResponse = {
-    statuscode: 200 // Simulate a successful transaction
-    // Add other properties if needed
-  }
-
-  console.log('dummyApiResponse:', dummyApiResponse)
-
-  // Check if the dummy API transaction was successful
-  if (dummyApiResponse.statuscode === 200) {
+  // Check if the API request was successful
+  if (status === 200) {
     // Retrieve the user from the database
-    const user = await User.findById(req.user.id)
-    console.log('user:', user)
+    const user = await User.findById(req.user.id);
+    console.log('user:', user);
     if (!user) {
-      res.status(400)
-      throw new Error('User not found')
+      res.status(400);
+      throw new Error('User not found');
     }
 
     // Check if the user's balance is sufficient
-    if (user.walletBalance < productAmount) {
-      res.status(400)
-      throw new Error('Insufficient balance')
+    if (user.walletBalance < amount) {
+      res.status(400);
+      throw new Error('Insufficient balance');
     }
 
     // Deduct the product price from the user's balance
-    user.walletBalance -= amount
+    user.walletBalance -= amount;
+    console.log('user.walletBalance:', user.walletBalance);
+
     // Get the user's package transaction level and percentage
-    const userPackage = await Package.findOne({ _id: user.package.ID })
-    console.log('userPackage:', userPackage)
-    const userTransactionLevel = userPackage.transaction.level
-    const userTransactionPercentage = user.userTransactionPercent
-
-    console.log('userTransactionLevel:', userTransactionLevel)
-    console.log('userTransactionPercentage:', userTransactionPercentage)
-
-    // // Calculate the bonus amount based on the product amount and transaction percentage
-    // const bonusAmount = (
-    //   (amount * userTransactionPercentage) /
-    //   100
-    // ).toFixed(2)
+    const userPackage = await Package.findOne({ _id: user.package.ID });
+    console.log('userPackage:', userPackage);
+    const userTransactionLevel = userPackage.transaction.level;
+    const userTransactionPercentage = user.userTransactionPercent;
+    console.log('userTransactionLevel:', userTransactionLevel);
+    console.log('userTransactionPercentage:', userTransactionPercentage);
 
     // Add the bonus amount to the user's balance
-    user.commissionBalance += Number(bonusAmount)
-    await user.save()
-    console.log('bonusAmount:', bonusAmount)
+    user.walletBalance += Number(bonusAmount);
+    await user.save();
+    console.log('user.walletBalance (after bonus):', user.walletBalance);
 
     // Check if the package has transaction levels defined
-    console.log(userPackage);
-    console.log(userPackage.transaction);
-    console.log(userPackage.transaction.level);
     if (userPackage && userPackage.transaction && userPackage.transaction.level > 0) {
       const transactionLevels = userPackage.transaction.level;
+      console.log('transactionLevels:', transactionLevels);
 
       // Determine the bonus levels based on the user's package and referralBonusLevel
       const bonusLevels = Math.min(
         transactionLevels,
         userPackage.referralBonusLevel
       );
+      console.log('bonusLevels:', bonusLevels);
 
       let uplineUser = user;
       for (let i = 1; i <= bonusLevels; i++) {
@@ -247,10 +414,10 @@ const purchaseData = asyncHandler(async (req, res) => {
         }
 
         // Find upline user and update their balance with transaction profit
-        const upline = await User.findById(uplineUser.upline.ID)
+        const upline = await User.findById(uplineUser.upline.ID);
+        console.log('upline:', upline);
         if (upline) {
           // Check if the upline user is a basic user
-          console.log(upline.package.name)
           if (upline.package.name === 'Basic' && i !== 1) {
             uplineUser = upline.upline;
             continue;
@@ -260,47 +427,49 @@ const purchaseData = asyncHandler(async (req, res) => {
             path: 'package.ID',
             select: 'transaction.percentage',
           });
+          console.log('uplinePercentage:', uplinePercentage);
 
-          console.log(uplinePercentage.package.ID.transaction.percentage);
           const transactionProfit = Number(
             (profit * uplinePercentage.package.ID.transaction.percentage) / 100
           ).toFixed(2);
-          console.log("Upline Tranaction Profit: ", transactionProfit);
+          console.log('transactionProfit:', transactionProfit);
 
-          upline.commissionBalance += Number(transactionProfit);
+          upline.walletBalance += Number(transactionProfit);
           await upline.save();
+          console.log('upline.walletBalance:', upline.walletBalance);
         }
 
         uplineUser = upline; // Move to the next upline user
       }
     }
 
-
     // If the transaction is successful, create a new credit transaction record for the data purchase
     const transaction = new Transaction({
       user: req.user.id,
-      amount: productAmount,
-      transactionId: 'hello', // Replace with actual transaction ID
+      amount: amount,
+      transactionId: transactionId, // Replace with actual transaction ID
       transactionType: 'data',
       commission: bonusAmount,
       status: 'completed',
-      dataPlan: productName,
+      dataPlan: network,
       phoneNumber: phoneNumber,
       network: network
-    })
+    });
+    console.log('transaction:', transaction);
 
-    await transaction.save()
+    await transaction.save();
 
     res.status(200).json({
       message: 'Data plan purchased successfully',
       bonusAmount
-    })
+    });
   } else {
     res.status(400).json({
-      message: 'Failed to purchase data plan'
-    })
+      message: data,
+      error: 'Failed to purchase data plan'
+    });
   }
-})
+});
 
 const walletTransfer = asyncHandler(async (req, res) => {
   const { senderUsername, recipientUsername, amount } = req.body
